4cc262d9b20d052dd2c483893d9da5816e45d24e
Sneha Vijayan <125389679+Snehavijayan555@users.noreply.github.com>
2024-10-24 22:59:52 +0530

    changed navbar and other changes

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,3 +1,3 @@
-document.getElementById('whatsapp-chat').onclick = function() {
+document.getElementsByClassName('fa-whatsapp').onclick = function() {
     window.open('https://wa.me/9562990198?text=Hi', '_blank');
 };
